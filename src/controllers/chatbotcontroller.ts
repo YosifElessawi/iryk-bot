b229f23fcc617ccd9b7a34b959a1628fe11ccd0c
YosifElessawi <yosifelessawi@yahoo.com>
2022-11-05 16:21:32 +0200

    handle message

--- src/controllers/chatbotcontroller.ts
+++ src/controllers/chatbotcontroller.ts
@@ -1,16 +1,54 @@
 import { Request, Response } from "express"
+import request from "request"
 import * as dotenv from "dotenv"
 dotenv.config()
 
-export const handleMessage: any = (
-  sender_psid: any,
-  received_message: any
-) => {}
+export const handleMessage: any = (sender_psid: any, received_message: any) => {
+  let response: { text: string } | undefined
+
+  // Check if the message contains text
+  if (received_message.text) {
+    // Create the payload for a basic text message
+    response = {
+      text: `You sent the message: "${received_message.text}". Now send me an image!`,
+    }
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response)
+}
+
 export const handlePostback: any = (
   sender_psid: any,
   received_message: any
 ) => {}
-export const callSendAPI: any = (sender_psid: any, received_message: any) => {}
+
+export const callSendAPI: any = (sender_psid: any, response: any) => {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v7.0/me/messages",
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!")
+      } else {
+        console.error("Unable to send message:" + err)
+      }
+    }
+  )
+}
 
 export const getWebHook = (req: Request, res: Response) => {
   // Get my verfiy token
@@ -43,7 +81,7 @@ export const postWebHook = (req: Request, res: Response) => {
 
   if (body.object === "page") {
     // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry: { messaging: any[] }) {
+    body.entry.forEach(function (entry) {
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0]
       console.log(webhook_event)
@@ -51,8 +89,15 @@ export const postWebHook = (req: Request, res: Response) => {
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id
       console.log("Sender PSID: " + sender_psid)
-    })
 
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message)
+      } else if (webhook_event.postback) {
+        handlePostback(sender_psid, webhook_event.postback)
+      }
+    })
     // Returns a '200 OK' response to all requests
     res.status(200).send("EVENT_RECEIVED")
   } else {
